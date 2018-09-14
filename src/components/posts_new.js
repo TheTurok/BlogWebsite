2f7bb4c68824ffa12fed6b6f8dd752b7c75b185d
Timothy Choi <TurokChoi@gmail.com>
2018-09-14 16:08:38 -0700

    two pages so far

---
+++ src/components/posts_new.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import { Field, reduxForm} from 'redux-form';
+
+class PostsNew extends Component {
+  renderField(field){
+    return(
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
+
+
+  render(){
+    return (
+      <form>
+        <Field
+          name="title"
+          component={this.renderField}
+          label="Title"
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Body"
+          name="content"
+          component={this.renderField}
+          />
+      </form>
+    );
+  }
+}
+
+function validate(values){
+
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew);
